0696ebbafdf53c7b41f7be90f70f0e04c45955c8
Johana Dominguez Medrano <32761613+johanadm23@users.noreply.github.com>
2026-03-29 23:30:42 -0500

    Add valid countries and disability types constants

---
+++ src/config.py
@@ -0,0 +1,6 @@
+VALID_COUNTRIES = ["USA", "India", "Brazil", "Germany", "Kenya", "Peru", "UK"]
+
+DISABILITY_TYPES = [
+    "none", "visual", "hearing", "motor",
+    "cognitive", "multiple", "prefer_not_to_say"
+]
